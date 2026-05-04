ed96ceedeb69e6217c6236a43c057fcdb49ddd17
Abdel792 <abdelkrim.bensaid@gmail.com>
2026-05-04 23:50:21 +0200

    Update changelog.

--- buildVars.py
+++ buildVars.py
@@ -29,8 +29,8 @@ Once configured, you can open a dialog that lets you start a new conversation or
 	addon_version="20260220.0.1",
 	# Brief changelog for this version
 	# Translators: what's new content for the add-on version to be shown in the add-on store
-	addon_changelog=_("""Changes for 20260301.0.0
-Updated documentation."""),
+	addon_changelog=_("""* Updated translations from Crowdin
+* Added support for NVDA 2026 API"""),
 	# Author(s)
 	addon_author="Abdelkrim BENSAÏD <abdelkrim.bensaid@gmail.com>",
 	# URL for the add-on documentation support
